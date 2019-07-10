34ed2cd162ea12183e43b64a39654e50585e7822
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-07-09 22:18:40 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,14 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; private set; } = new List<Seller>();
 
         public Department()
         {
@@ -13,6 +18,16 @@
         {
             Id = id;
             Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Select(s => s.TotalSales(initial, final)).Aggregate(0.0, (sale1, sale2) => sale1 + sale2);
         }
     }
 }
